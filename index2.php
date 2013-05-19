9ec458b92b6f8ede1f51aeb96a0a2d5085e7528f
TAKAICHI NISHIDA <nishida@aainc.co.jp>
2013-05-20 00:24:05 +0900

    change

--- index2.php
+++ index2.php
@@ -5,7 +5,9 @@
     $username = $url["user"];
     $password = $url["pass"];
     $db = substr($url["path"],1);
+    echo '1';
     mysql_connect('localhost', 'root','');
+    echo '2';
     mysql_select_db('people');
     $rs=mysql_query("select * from people" );
     while($dat=mysql_fetch_assoc($rs)){echo "[id]:".$dat["id"]." [name]:".$dat["name"]."<br />";};
